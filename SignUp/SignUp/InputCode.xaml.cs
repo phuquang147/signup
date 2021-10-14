901da15092ff1d198a9debb5125d7979fec780ae
Phu Quang <phuquang14722@gmail.com>
2021-10-14 20:44:05 +0700

    update

--- SignUp/SignUp/InputCode.xaml.cs
+++ SignUp/SignUp/InputCode.xaml.cs
@@ -23,5 +23,10 @@ namespace SignUp
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
     }
 }
